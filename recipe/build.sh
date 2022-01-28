b58c2e8a767295c56b8843608587e398032edf4c
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-28 01:30:29 -0800

    Use PYTHON variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,6 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja
 ninja install   # Install C++ library into Conda env
 cd ../python
-python setup.py install --single-version-externally-managed --record=record.txt
+${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
 cd ../runtime/python
-python setup.py install --single-version-externally-managed --record=record.txt
+${PYTHON} setup.py install --single-version-externally-managed --record=record.txt

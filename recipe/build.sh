34457a1c4e87f10eb5b8035997590001a681b759
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-30 13:24:47 -0800

    Pass CMake args to cross-compile (#41)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -e
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja
+cmake ${CMAKE_ARGS} .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja
 ninja install   # Install C++ library into Conda env
 cd ../python
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt

2c4f0ad092e5632895c94df690850247bc1542e2
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-31 12:15:49 -0700

    Make CMake find llvm-openmp

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -e
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY"
+cmake ${CMAKE_ARGS} .. -DCMAKE_PREFIX_PATH="$PREFIX" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY"
 ninja install   # Install C++ library into Conda env
 cd ../python
 ${PYTHON} -m pip install -v . --config-settings use_system_libtreelite=True

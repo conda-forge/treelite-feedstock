53d2ab8397302fdd7b5a1f45d3a744897441eeba
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-31 12:59:44 -0700

    Specify OpenMP flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,15 @@ set -e
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} .. -DCMAKE_PREFIX_PATH="$PREFIX" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY"
+
+if [[ "$target_platform" == osx-* ]]
+then
+  OPENMP_FLAGS="-DOpenMP_C_FLAG=-fopenmp -DOpenMP_CXX_FLAG=-fopenmp -DOpenMP_C_LIB_NAMES=libomp -DOpenMP_CXX_LIB_NAMES=libomp"
+else
+  OPENMP_FLAGS=""
+fi
+
+cmake ${CMAKE_ARGS} .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" ${OPENMP_FLAGS}
 ninja install   # Install C++ library into Conda env
 cd ../python
 ${PYTHON} -m pip install -v . --config-settings use_system_libtreelite=True
